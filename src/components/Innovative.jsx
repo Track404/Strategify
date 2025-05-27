764db1e8e57f43eeb97d57066cea11c3621a4ac0
Track404 <111713000+Track404@users.noreply.github.com>
2025-05-27 15:13:47 +0200

    creation of the innative part of the landing page

---
+++ src/components/Innovative.jsx
@@ -0,0 +1,75 @@
+import strategy from '../assets/strategy.jpg';
+import webinterface from '../assets/webinterface.jpg';
+import analytics from '../assets/analytics.jpg';
+function InnovativePart() {
+  return (
+    <div className="hero bg-white text-neutral min-h-screen lg:p-5 ">
+      <div className="hero-content flex-col  text-center ">
+        <div className="text-success text-lg">Innovative</div>
+        <div>
+          <h1 className="text-4xl sm:text-5xl font-bold capitalize">
+            Transform Your Marketing Strategy With Ease
+          </h1>
+          <p className="py-6 text-lg sm:text-xl">
+            We simplify the marketing palnning process, allowing you to focus on
+            what matters most growing your business and connecting with your
+            audience.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="card  shadow-md bg-gray-100 p-1 w-[75vw] lg:w-[30vw] ">
+            <figure>
+              <img src={strategy} alt="Marketing Strategy" />
+            </figure>
+            <div className="card-body ">
+              <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl ">
+                User-Friendly Interface for Seamless Experience
+              </h2>
+              <p className="md:text-lg xl:text-xl 2xl:text-2xl">
+                Oour intuitive platform makes creating marketing strategies
+                simple and accessible for everyone, regardless of experience
+                level.
+              </p>
+            </div>
+          </div>
+          <div className="card  shadow-md bg-gray-100 p-1 w-[75vw] lg:w-[30vw] ">
+            <figure>
+              <img src={webinterface} alt="Web Interface" />
+            </figure>
+            <div className="card-body ">
+              <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl">
+                Tailored Solutions for Every Buisiness Type
+              </h2>
+              <p className="md:text-lg xl:text-xl 2xl:text-2xl">
+                Get customized marketing strategies that align with your
+                specific, buisness goals, and target audience.
+              </p>
+            </div>
+          </div>
+          <div className="card  shadow-md bg-gray-100 p-1 w-[75vw] lg:w-[30vw] ">
+            <figure>
+              <img src={analytics} alt="Data Analytics" />
+            </figure>
+            <div className="card-body ">
+              <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl">
+                Data-Driven Insights for Informed Decisions
+              </h2>
+              <p className="md:text-lg xl:text-xl 2xl:text-2xl">
+                Leverage powerful analytics and market research to create
+                strategies backed by real data and proven methodologies.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8">
+          <button className="btn btn-primary md:btn-lg">Explore</button>
+          <button className="btn btn-dash btn-secondary md:btn-lg ml-4">
+            Start Free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InnovativePart;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar';
 import Homepage from '../components/HomePage';
 import ProcessPart from '../components/Process';
+import InnovativePart from '../components/Innovative';
 function LandingPage() {
   return (
     <div className="bg-white h-full relative">
       <Navbar />
       <Homepage />
       <ProcessPart />
+      <InnovativePart />
     </div>
   );
 }
