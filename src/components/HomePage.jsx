3142ec3e7de732ebb2b15fc313a4850e2c013159
Track404 <111713000+Track404@users.noreply.github.com>
2025-05-27 17:33:25 +0200

    add a background image to the home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,25 +1,24 @@
-import homepagePicture from '../assets/HomepagePicture.jpg';
-
+import BackgroundHome from '../assets/BackgroundHome.jpg';
 function Homepage() {
   return (
-    <div className="hero bg-base text-neutral min-h-screen lg:p-5 ">
-      <div className="hero-content flex-col lg:flex-row-reverse text-center lg:text-start">
-        <img
-          src={homepagePicture}
-          alt="Homepage Illustration"
-          className="max-w-sm mb-5 rounded-lg shadow-xl w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px] 2xl:w-[600px]2xl:h-[600px] object-cover "
-        />
+    <div
+      className="hero bg-base min-h-screen lg:p-5 relative"
+      style={{
+        backgroundImage: `linear-gradient(135deg, rgba(0,0,0,0.5), rgba(0,0,0,0.3)), url(${BackgroundHome})`,
+      }}
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse text-center text-neutral-content  ">
         <div>
-          <h1 className="text-4xl sm:text-5xl font-bold capitalize">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold capitalize text-white">
             Transform your marketing strategy in minutes
           </h1>
-          <p className="py-6 text-lg sm:text-xl">
+          <p className="py-6 text-lg sm:text-xl lg:text-2xl font-semibold text-white">
             Start for free! Fill out a simple form, get an instant preview of
             your customized marketing strategy, and upgrade only when you're
             ready for the full plan.
           </p>
           <button className="btn btn-primary md:btn-lg">Start For Free</button>
-          <button className="btn btn-dash btn-secondary md:btn-lg ml-4">
+          <button className="btn btn-secondary md:btn-lg ml-4">
             Learn More
           </button>
         </div>
