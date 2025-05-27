6a204875e75d640af7f963a1808ed1df34942345
Track404 <111713000+Track404@users.noreply.github.com>
2025-05-27 16:50:30 +0200

    creation of the footer for the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import ProcessPart from '../components/Process';
 import InnovativePart from '../components/Innovative';
 import PricingPart from '../components/Pricing';
 import CallToAction from '../components/CallToAction';
+import Footer from '../components/Footer';
 function LandingPage() {
   return (
     <div className="bg-white h-full relative">
@@ -13,6 +14,7 @@ function LandingPage() {
       <InnovativePart />
       <PricingPart />
       <CallToAction />
+      <Footer />
     </div>
   );
 }
